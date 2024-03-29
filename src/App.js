4980bdd43d9a12f11424966cf0e1410787b2e58d
omarnaeem0 <omar_naeem@live.com>
2024-03-29 11:05:59 +0500

    temp

--- src/App.js
+++ src/App.js
@@ -8,14 +8,25 @@ function App() {
   const [results, setResults] = useState([]);
   const [error, setError] = useState(null);
   useEffect(() => {
+    setLoading(true);
     fetch(
       "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=2x4KUAcLSwFMuIZxVpNyTb2GYSyi9tvS"
     )
       .then((res) => res.json())
       .then((data) => {
         setResults(data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
       });
   }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
   console.log("==========", results);
   return (
     <div className="App">
